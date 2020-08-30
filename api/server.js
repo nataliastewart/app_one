9edbbddf91e49a9a965b507c85fcd6a523ae097b
Natalia <nataliamirandac@hotmail.com>
2020-08-30 12:21:18 -0400

    Creates users-model and users-router

--- api/server.js
+++ api/server.js
@@ -3,12 +3,17 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 //routers
+const usersRouter = require("../users/users-router.js");
+
 const server = express();
 
 //middleware
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+//use routers
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "started" });
