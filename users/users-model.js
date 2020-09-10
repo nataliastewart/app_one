5898e3e118ef0c963edf2c1d6efdaefaddec4b5a
Natalia <nataliamirandac@hotmail.com>
2020-09-09 20:27:37 -0400

    knexfile updated

--- users/users-model.js
+++ users/users-model.js
@@ -18,13 +18,13 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  try {
-    const [id] = await db("users").insert(user);
+  // try {
+  const [id] = await db("users").insert(user, "id");
 
-    return findById(id);
-  } catch (error) {
-    throw error;
-  }
+  return findById(id);
+  // } catch (error) {
+  //   throw error;
+  // }
 }
 
 function findById(id) {
